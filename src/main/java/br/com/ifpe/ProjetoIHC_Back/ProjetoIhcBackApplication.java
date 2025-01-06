cca5fb26f5c9563c9e82616538123889d01e71a7
Jaidene silva <jaidene_silva@hotmail.com>
2025-01-06 14:55:51 -0300

    configurações iniciais

---
+++ src/main/java/br/com/ifpe/ProjetoIHC_Back/ProjetoIhcBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.ProjetoIHC_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIhcBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIhcBackApplication.class, args);
+	}
+
+}
